d9aa81fe227042399208b0b768f45b933abc9feb
Syed Maruf Ali <sdmrfa@Syeds-MacBook-Air.local>
2025-06-30 11:09:01 +0530

    workflow updated

--- apps/workers/opt-out-td-webhook/src/functions/webhook.ts
+++ apps/workers/opt-out-td-webhook/src/functions/webhook.ts
@@ -52,6 +52,6 @@ export async function optOutWebhook(
 app.http("OptOutWebhook", {
   methods: ["GET", "POST"],
   authLevel: "anonymous",
-  route: "webhook/opt-out-hs",
+  route: "webhook/opt-out-td",
   handler: optOutWebhook,
 });
